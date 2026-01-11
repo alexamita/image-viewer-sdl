5d691100e66d8f9a23fd871bb73f5a36e01782e6
Alex Amita <alexamita@AA-MBP.local>
2026-01-11 04:35:59 +0300

    PPM header handling and optimised SDL pixel rendering

--- iv.c
+++ iv.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <SDL2/SDL.h>
 
 int main() {
-    const int width = 900;
-    const int height = 600;
+    FILE *in = stdin;
+    char line[1024];
+
+    // Read magic number (P6 only) from PPM header
+    fgets(line, sizeof(line), in);
+    if (line[0] != 'P' || line[1] != '6') {
+        fprintf(stderr, "Only P6 PPM format supported\n");
+        return 1;
+    }
+
+    //Read width and height (skip comments)
+    do {
+        fgets(line, sizeof(line), in);
+    } while (line[0] == '#');
+
+    int width = -1;
+    int height = -1;
+    sscanf(line, "%d %d", &width, &height);
+
+    // Read max color value (skip comments)
+    do {
+        fgets(line, sizeof(line), in);
+    } while (line[0] == '#');
 
     //1. Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -24,29 +46,49 @@ int main() {
         SDL_Quit();
         return 1;
     }
-    int running = 1;
-    SDL_Event event;
 
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = 0;
-            }
-        }
-
-        // Get the window surface
+    // Get the window surface
         SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
-        // Fill the surface with red color
-        Uint32 color = SDL_MapRGB(psurface->format, 255, 0, 0);
-        SDL_Rect pixel = (SDL_Rect){0, 0, 1, 1};
-        SDL_FillRect(psurface, NULL, color);
+    // Lock the surface if needed
+    if (SDL_MUSTLOCK(psurface)) {
+        SDL_LockSurface(psurface);
+    }
 
-        // Update the window surface to reflect changes
-        SDL_UpdateWindowSurface(pwindow);
+    // Read pixel data from stdin and populate the surface
+    Uint32 *pixels = (Uint32 *)psurface->pixels;
+    // Calculate pitch in terms of number of pixels
+    int pitch = psurface->pitch / 4;
+    // Read pixel data
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Uint8 r = getchar();
+            Uint8 g = getchar();
+            Uint8 b = getchar();
+            // Map RGB to pixel format and set pixel
+            pixels[y * pitch + x] = SDL_MapRGB(psurface->format, r, g, b);
+        }
+    }
+
+    // Unlock the surface if it was locked
+    if (SDL_MUSTLOCK(psurface)) {
+    SDL_UnlockSurface(psurface);
+    }
+
+    // Update the window surface to reflect changes
+    SDL_UpdateWindowSurface(pwindow);
+
+    int app_running = 1;
+    SDL_Event event;
+
+    while (app_running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                app_running = 0;
+            }
+        }
         //Pause for 16 milliseconds after each loop iteration to prevent CPU burn (~60 FPS)
         SDL_Delay(16);
-
     }
     //3. Clean up and quit SDL
     SDL_DestroyWindow(pwindow);
